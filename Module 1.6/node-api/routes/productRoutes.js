7ba542afa2365a0e10d213ee6e9d353cabf4523d
Andres Aguilar <andres.aguilar@unosquare.com>
2022-08-16 14:01:11 -0500

    node api course

---
+++ Module 1.6/node-api/routes/productRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const productController = require('../controller/productController');
+const joiSchemaValidation = require('../middleware/joiSchemaValidation');
+const productSchema = require('../apiSchema/productSchema');
+
+router.post('/', joiSchemaValidation.validateBody(productSchema.createProductSchema), productController.createProduct);
+
+router.get('/', joiSchemaValidation.validateQueryParams(productSchema.getAllProductsSchema), productController.getAllProducts);
+
+router.get('/:id', productController.getProductById);
+
+router.put('/:id', joiSchemaValidation.validateBody(productSchema.updateProductSchema), productController.updateProduct)
+
+module .exports = router;
---
+++ Module 1.6/notes/mongo.md
@@ -0,0 +1,3 @@
+For running mongo and stop it:
+
+https://www.mongodb.com/docs/manual/tutorial/install-mongodb-on-os-x/#run-mongodb-community-edition
