6f903d761da0d6dba93d6074055f9895e2adcb1a
Andres Aguilar <andres.aguilar@unosquare.com>
2022-09-26 17:20:49 -0500

    project compleated

--- Module 1.5/music/src/main.js
+++ Module 1.5/music/src/main.js
@@ -4,10 +4,19 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 import VeeValidatePlugin from "./includes/validation";
+import Icon from "./directives/icon.js";
 import { auth } from "./includes/firebase";
+import { registerSW } from "virtual:pwa-register";
+import GlobalComponents from "./includes/_globals.js";
+import progressBar from "@/includes/progress-bar.js";
 
 import "./assets/base.css";
 import "./assets/main.css";
+import "nprogress/nprogress.css";
+
+registerSW({ immediate: true });
+
+progressBar(router);
 
 let app;
 
@@ -18,6 +27,8 @@ auth.onAuthStateChanged(() => {
     app.use(createPinia());
     app.use(router);
     app.use(VeeValidatePlugin);
+    app.use(GlobalComponents);
+    app.directive("icon", Icon);
 
     app.mount("#app");
   }
--- Module 1.5/music/src/views/HomeView.vue
+++ Module 1.5/music/src/views/HomeView.vue
@@ -29,12 +29,12 @@
       <div
         class="bg-white rounded border border-gray-200 relative flex flex-col"
       >
-        <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
+        <div
+          class="px-6 pt-6 pb-5 font-bold border-b border-gray-200"
+          v-icon-secondary="{ icon: 'headphones-alt', right: true }"
+        >
           <span class="card-title">Songs</span>
           <!-- Icon -->
-          <i
-            class="fa fa-headphones-alt float-right text-green-400 text-xl"
-          ></i>
         </div>
         <!-- Playlist -->
         <ol id="playlist">
@@ -48,12 +48,16 @@
 <script>
 import { songsCollection } from "@/includes/firebase.js";
 import SongItem from "@/components/SongItem.vue";
-import { max } from "@vee-validate/rules";
+import IconSecondary from "@/directives/icon-secondary.js";
+// import { max } from "@vee-validate/rules";
 
 export default {
   name: "HomeView",
   components: {
     SongItem,
+  },
+  directives: {
+    "icon-secondary": IconSecondary,
   },
   data() {
     return {
