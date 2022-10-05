38399a790a7c983c3e48c56c55153354236cc934
Andres Aguilar <andres.aguilar@unosquare.com>
2022-10-05 15:34:02 -0500

    add viewsets

--- Module 1.8/profiles-rest-api/profiles_api/views.py
+++ Module 1.8/profiles-rest-api/profiles_api/views.py
@@ -1,4 +1,5 @@
 from rest_framework.views import APIView
+from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -42,3 +43,48 @@ class HelloApiView(APIView):
     def delete(self, request, pk=None):
         """Delete an object"""
         return Response({'method': 'DELETE'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet"""
+    serializer_class = serializers.HelloSerializer
+
+    def list(self, request):
+        """Hello message"""
+
+        a_viewset = ['Uses actions (list, create, retrieve, update, partial_update, destroy)',
+                     'Automatically maps urls using routers',
+                     'Provides more functionalities with less code']
+
+        return Response({'message': 'Hello', 'a view set': a_viewset})
+
+    def create(self, request):
+        """Create a new hello message"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}!'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def retrieve(self, request, pk=None):
+        """Handle getting an object by its ID"""
+        return Response({'http method': 'GET'})
+
+    def update(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({'http method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """Handle partial update of an object"""
+        return Response({'http method': 'PATCH'})
+
+    def destroy(self, request, pk=None):
+        """Handle removing an object"""
+        return Response({'http method': 'DELETE'})
+    
