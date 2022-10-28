536562b1eed063458307be63c9b9e469e714e883
Andres Aguilar <andres.aguilar@unosquare.com>
2022-10-28 14:42:01 -0500

    excercise compleated

--- Module 1.7/tests/checkout.spec.js
+++ Module 1.7/tests/checkout.spec.js
@@ -38,4 +38,8 @@ it('Can apply discount rules to the total', function(){
     checkout.addItem('a');
     checkout.addItem('a');
     expect(checkout.calculateTotal()).to.equal(2);
-})
+});
+
+it('Throws when item added with no price', function(){
+    expect(function (){checkout.addItem('c')}).to.throw();
+});
