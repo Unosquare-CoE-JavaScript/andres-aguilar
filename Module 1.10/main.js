178547c96cbb1ff1501a14d9422e1843f40b4afb
Andres Aguilar <andres.aguilar@unosquare.com>
2022-11-04 15:46:51 -0600

    refactor to promise

--- Module 1.10/main.js
+++ Module 1.10/main.js
@@ -1,21 +1,46 @@
 const https = require('https');
 
-function getData(episode) {
-    let info = [];
-    https.get(`https://swapi.dev/api/planets/${episode}/`, (response) => {
-    let data = '';
-    response.on('data', (chunk) => {
-        data = JSON.parse(chunk);
-        info = Object.keys(data).map((key) => [data[key]]);
-    });
-
-    response.on('end', () => {
-        console.log(info);
-    });
- })
-    .on('error', (error) => {
-        console.log(error);
+async function getPlanet(id) {
+    return new Promise((resolve, reject) => {
+        let data = '';
+        https.get(`https://swapi.dev/api/planets/${id}/`, (response) => {
+        response.on('data', (chunk) => {
+            data = JSON.parse(chunk);
+            // info = Object.keys(data).map((key) => [data[key]]);
+        });
+        response.on('end', () => {
+            resolve(data.residents[1])
+        });
+     })
+        .on('error', (error) => {
+            console.log(error);
+        })
     })
 };
 
-getData(1);
+async function getChar(url) {
+    return new Promise((resolve, reject) => {
+        let character = [];
+        https.get(url, (response) => {
+        let data = '';
+        response.on('data', (chunk) => {
+            data = JSON.parse(chunk);
+            // info = Object.keys(data).map((key) => [data[key]]);
+            character = [data.name, data.height]
+        });
+        response.on('end', () => {
+            console.log(character)
+        });
+        })
+        .on('error', (error) => {
+            console.log(error);
+        })
+        resolve(character)
+    })
+};
+
+async function getData() {
+    await getPlanet(1).then((res) => getChar(res))
+}
+
+getData();
