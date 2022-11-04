fa2fc1325aca18a9917d5e7fb90b112aabc0c459
Andres Aguilar <andres.aguilar@unosquare.com>
2022-11-04 14:17:38 -0600

    add request to aray

--- Module 1.10/main.js
+++ Module 1.10/main.js
@@ -1,14 +1,16 @@
 const https = require('https');
 
 function getData(episode) {
-    https.get('https://swapi.dev/api/films/1/', (response) => {
+    let info = [];
+    https.get(`https://swapi.dev/api/planets/${episode}/`, (response) => {
     let data = '';
     response.on('data', (chunk) => {
         data = JSON.parse(chunk);
+        info = Object.keys(data).map((key) => [data[key]]);
     });
 
     response.on('end', () => {
-        console.log(info.title);
+        console.log(info);
     });
  })
     .on('error', (error) => {
@@ -16,3 +18,4 @@ function getData(episode) {
     })
 };
 
+getData(1);
